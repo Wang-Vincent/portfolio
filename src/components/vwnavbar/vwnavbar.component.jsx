33eb3cc9b3d816cd05e7360fef84b6006968cb13
vwnightmare@gmail.com <Vw19920805>
2021-01-03 17:40:17 -0800

    initial commit

---
+++ src/components/vwnavbar/vwnavbar.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "./vwnavbar.style.css"
+
+const VWNavbar = () => {
+    return (
+        <>
+            <Navbar fixed="top" collapseOnSelect expand="md" variant="dark" className="animate-navbar nav-color justify-content-between">
+                <Navbar.Brand href="#home">Vincent Wang</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#skills">Skills</Nav.Link>
+                        <Nav.Link href="#experience">Experience</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
+                        <Nav.Link href="#resume">Resume</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
+    )
+}
+
+export default VWNavbar;
